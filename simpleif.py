6eef2f88f932f90787e2cbfaea8a4e821b07eed9
pawarssujit <139035732+pawarssujit@users.noreply.github.com>
2023-08-06 23:16:30 +0530

    Add files via upload

---
+++ simpleif.py
@@ -0,0 +1,9 @@
+#if else condition
+#Enter two numbers and display max
+a = int(input("Enter your no:"))
+b = int(input("enter your no:"))
+if b>a:
+    print("MAX=",b)
+else:
+    print("MAX=",a)
+
