0a1123edae9873a4859a36d9b7302492f7609529
pawarssujit <139035732+pawarssujit@users.noreply.github.com>
2023-08-08 23:59:02 +0530

    Add files via upload

---
+++ listmethods.py
@@ -0,0 +1,24 @@
+#sort the list
+l1=[1,8,10,9,2,4,3]
+l1.sort()
+print(l1)
+
+
+#reverse the list
+l1.reverse()
+print(l1)
+
+#adds at the ends of the list
+l1.append(15)
+print(l1)
+
+#add values in  the list
+l1.insert(0,55)
+print(l1)
+
+# delete the value
+l1.pop(2)
+print(l1)
+l1.remove(3)
+print(l1)
+
