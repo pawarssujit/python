88f50e52089ca18c7cd5a37cf02d66cd5738f5c0
pawarssujit <139035732+pawarssujit@users.noreply.github.com>
2023-08-16 01:42:39 +0530

    Add files via upload

---
+++ spam_conditions.py
@@ -0,0 +1,14 @@
+text= input("enter the text\n")
+
+if("make a lot of money" in text):
+    spam=True
+elif("buy now" in text):
+    spam=True
+elif("subscribe this" in text):
+    spam=True
+else:
+    spam= False
+if(spam):
+    print("This text is spam")
+else:
+    print("This text is not spam")
