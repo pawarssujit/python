b6508df18106c7c734fa04c53c197774a7c968e6
pawarssujit <139035732+pawarssujit@users.noreply.github.com>
2023-08-19 01:26:20 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,21 @@
+#1)
+i = 1
+
+while i <= 50:
+    print(i)
+    i = i + 1
+
+#2)
+i=0
+
+while i<5:
+    print("sujit")
+    i=i+1
+
+#3)
+fruits= ["banana","watermelon","apple","mangoe"]
+
+i=0
+while i<len(fruits):
+        print(fruits[i])
+        i=i+1
